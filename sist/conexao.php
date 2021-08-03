5ff5a654838c77e995d1b88d7182d60fed804e26
Gilmorius <joaoeduardo.008@gmail.com>
2021-08-03 17:45:17 -0300

    commit secundario

--- sist/conexao.php
+++ sist/conexao.php
@@ -2,17 +2,17 @@
   $host = 'localhost';
   $user = 'root';
   $passwd = 'vertrigo';
-  $bd = 'sistema';
+  $bd = 'easyMarket';
   $conn = mysqli_connect($host, $user, $passwd, $bd);
 	if(!$conn) {
 		die('Falha ao conectar no servidor: ' . mysqli_error());
 	}
 
-	mysqli_query($link, "SET SESSION sql_mode='';");
-	mysqli_set_charset($link, 'utf8');
-	mysqli_query($link, "SET NAMES 'utf8';");
-	mysqli_query($link, "SET CHARACTER SET 'utf8';");
-	mysqli_query($link, "SET COLLATION_CONNECTION = 'utf8_unicode_ci';");
+	mysqli_query($conn, "SET SESSION sql_mode='';");
+	mysqli_set_charset($conn, 'utf8');
+	mysqli_query($conn, "SET NAMES 'utf8';");
+	mysqli_query($conn, "SET CHARACTER SET 'utf8';");
+	mysqli_query($conn, "SET COLLATION_CONNECTION = 'utf8_unicode_ci';");
 
 	date_default_timezone_set('America/Sao_Paulo');
 	setlocale(LC_TIME, 'portuguese');
