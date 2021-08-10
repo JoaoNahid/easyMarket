b1df93ceff4546786805603bad670bcb9f52ab64
Leonardo Silva <lpssilva644@gmail.com>
2021-08-10 15:20:46 -0300

    Cadastro de Produtos (10/08 às 15:20)

--- sist/inserirProduto.php
+++ sist/inserirProduto.php
@@ -13,7 +13,7 @@ if (isset($_GET['idProduto'])) {
     $categoriaProduto = $row['idCategoria'];
     $precoProduto = $row['precoProduto'];
     $precoPromocao = $row['precoPromocao'];
-    $fotoProduto = $row['fotoProduto'];
+    $fotoBanco = $row['fotoProduto'];
     $pesoProduto = $row['pesoProduto'];
     $localizacaoProduto = $row['localizacaoProduto'];
     $descricaoProduto = $row['descricaoProduto'];
@@ -36,7 +36,6 @@ if(isset($_POST['cadastrar'])){
 
   $codigoProduto = htmlspecialchars($_POST['codigoProduto'], ENT_QUOTES, 'utf-8');
   $nomeProduto = htmlspecialchars($_POST['nomeProduto'], ENT_QUOTES, 'utf-8');
-  $fotoProduto = htmlspecialchars($_POST['fotoProduto'], ENT_QUOTES, 'utf-8');
   $marcaProduto = htmlspecialchars($_POST['marcaProduto'], ENT_QUOTES, 'utf-8');
   $categoriaProduto = htmlspecialchars($_POST['idCategoria'], ENT_QUOTES, 'utf-8');
   $precoProduto = htmlspecialchars($_POST['precoProduto'], ENT_QUOTES, 'utf-8');
@@ -48,60 +47,6 @@ if(isset($_POST['cadastrar'])){
   $destaqueProduto = htmlspecialchars($_POST['destaqueProduto'], ENT_QUOTES, 'utf-8');
 
 
-  $msg = false;
-if(isset($_FILES['arquivo'])){
-    $arquivo = $_FILES['arquivo']['name'];
-    $extensao = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
-
-    $novo_nome = md5(time()).".".$extensao;
-
-    $diretorio = "upload/";
-
-    move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio . $novo_nome);
-
-    $sql_code = "INSERT INTO arquivo(id, arquivo, data) VALUES('','$novo_nome', NOW())";
-
-    if(mysqli_query($conn, $sql_code))
-        $msg = "Arquivo enviado com sucesso!";
-    else
-        $msg = "Falha ao enviar arquivo!";
-}
-  else {
-    $foto = '';
-  }
-
-  if($status == 'vendido'){
-    $vendido = 'sim';
-  }
-  else {
-    $vendido = '';
-  }
-
-  $query = "INSERT INTO produtos (codigoProduto, nomeProduto, fotoProduto, marcaProduto, idCategoria, precoProduto, precoPromocao, pesoProduto, localizacaoProduto, avaliacaoProduto, descricaoProduto, destaqueProduto) VALUES ('$codigoProduto', '$nomeProduto', '$fotoProduto', '$marcaProduto', '$categoriaProduto', '$precoProduto', '$precoPromocao', '$pesoProduto', '$localizacaoProduto', '$avaliacaoProduto', '$descricaoProduto', '$destaqueProduto', '')";
-  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-  if (mysqli_affected_rows($conn)) {
-    header('Location: bazar21.php?Operacao realizada com sucesso');
-  }
-  else{
-    echo '
-      <script>
-        window.alert("Erro ao salvar, tente novamente!")
-      </script>
-    ';
-  }
-
-}
-if(isset($_POST['salvar'])){
-  $codigoProduto = htmlspecialchars($_POST['codigoProduto'], ENT_QUOTES, 'utf-8');
-  $categoria = htmlspecialchars($_POST['categoria'], ENT_QUOTES, 'utf-8');
-  $preco = htmlspecialchars($_POST['preco'], ENT_QUOTES, 'utf-8');
-  $sexo = htmlspecialchars($_POST['sexo'], ENT_QUOTES, 'utf-8');
-  $cor = htmlspecialchars($_POST['cor'], ENT_QUOTES, 'utf-8');
-  $tamanho = htmlspecialchars($_POST['tamanho'], ENT_QUOTES, 'utf-8');
-  $status = htmlspecialchars($_POST['status'], ENT_QUOTES, 'utf-8');
-  $descricao = htmlspecialchars($_POST['descricao'], ENT_QUOTES, 'utf-8');
-  $estado = htmlspecialchars($_POST['estado'], ENT_QUOTES, 'utf-8');
-
   if(isset($_FILES['imagem'])){
     $imagem = $_FILES['imagem']['name'];
     $extensao = strtolower(pathinfo($imagem, PATHINFO_EXTENSION));
@@ -112,7 +57,7 @@ if(isset($_POST['salvar'])){
 
       if(move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$novoNome)){
 
-        $foto = $novoNome;
+        $fotoProduto = $novoNome;
       }
       else {
         exit();
@@ -127,20 +72,70 @@ if(isset($_POST['salvar'])){
     }
   }
   else {
-    $foto = '';
+    $fotoProduto = '';
   }
 
-  if($status == 'vendido'){
-    $vendido = 'sim';
-  }
-  else {
-    $vendido = '';
-  }
 
-  $query = "UPDATE produtos SET produto='$produto', categoria='$categoria', sexo='$sexo', preco='$preco', cor='$cor', tamanho='$tamanho', estado='$estado', descricao='$descricao', status='$status', vendido='$vendido', foto='$foto' WHERE idProduto = '$idProduto'";
+  $query = "INSERT INTO produtos (codigoProduto, nomeProduto, fotoProduto, marcaProduto, idCategoria, precoProduto, precoPromocao, pesoProduto, localizacaoProduto, avaliacaoProduto, descricaoProduto, destaqueProduto, removido) VALUES ('$codigoProduto', '$nomeProduto', '$fotoProduto', '$marcaProduto', '$categoriaProduto', '$precoProduto', '$precoPromocao', '$pesoProduto', '$localizacaoProduto', '$avaliacaoProduto', '$descricaoProduto', '$destaqueProduto', '')";
   $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
   if (mysqli_affected_rows($conn)) {
-    header('Location: bazar21.php?Alteracao realizada com sucesso');
+    header('Location: listaProdutos.php?Operacao realizada com sucesso');
+  }
+  else{
+    echo '
+      <script>
+        window.alert("Erro ao salvar, tente novamente!")
+      </script>
+    ';
+  }
+
+}
+if(isset($_POST['salvar'])){
+  $codigoProduto = htmlspecialchars($_POST['codigoProduto'], ENT_QUOTES, 'utf-8');
+  $nomeProduto = htmlspecialchars($_POST['nomeProduto'], ENT_QUOTES, 'utf-8');
+  $fotoProduto = htmlspecialchars($_POST['fotoProduto'], ENT_QUOTES, 'utf-8');
+  $marcaProduto = htmlspecialchars($_POST['marcaProduto'], ENT_QUOTES, 'utf-8');
+  $categoriaProduto = htmlspecialchars($_POST['idCategoria'], ENT_QUOTES, 'utf-8');
+  $precoProduto = htmlspecialchars($_POST['precoProduto'], ENT_QUOTES, 'utf-8');
+  $precoPromocao = htmlspecialchars($_POST['precoPromocao'], ENT_QUOTES, 'utf-8');
+  $pesoProduto = htmlspecialchars($_POST['pesoProduto'], ENT_QUOTES, 'utf-8');
+  $localizacaoProduto = htmlspecialchars($_POST['localizacaoProduto'], ENT_QUOTES, 'utf-8');
+  $avaliacaoProduto = htmlspecialchars($_POST['avaliacaoProduto'], ENT_QUOTES, 'utf-8');
+  $descricaoProduto = htmlspecialchars($_POST['descricaoProduto'], ENT_QUOTES, 'utf-8');
+  $destaqueProduto = htmlspecialchars($_POST['destaqueProduto'], ENT_QUOTES, 'utf-8');
+
+  $fotoProduto = $fotoBanco;
+  if(isset($_FILES['imagem'])){
+    $imagem = $_FILES['imagem']['name'];
+    $extensao = strtolower(pathinfo($imagem, PATHINFO_EXTENSION));
+    $formatosImagem = array('png', 'jpg', 'jpeg', 'gif');
+    if (in_array($extensao, $formatosImagem)) {
+      $novoNome = md5(time()).rand(1000,99999).'.'.$extensao;
+      $diretorio = 'uploads/';
+
+      if(move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$novoNome)){
+
+        $fotoProduto = $novoNome;
+      }
+      else {
+        exit();
+      }
+    }
+    else{
+      echo '
+        <script>
+          window.alert("Formato de imagem não suportado, escolha entre .jpg, .jpeg, .png, ou .gif")
+        </script>
+      ';
+    }
+  }
+
+
+
+  $query = "UPDATE produtos SET codigoProduto='$codigoProduto', nomeProduto='$nomeProduto', fotoProduto='$fotoProduto', marcaProduto='$marcaProduto', categoriaProduto='$idCategoria', precoProduto='$precoProduto', precoPromocao='$precoPromocao', pesoProduto='$pesoProduto', localizacaoProduto='$localizacaoProduto', avaliacaoProduto='$avaliacaoProduto', descricaoProduto='$descricaoProduto', destaqueProduto= '$destaqueProduto' WHERE idProduto = '$idProduto'";
+  $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  if (mysqli_affected_rows($conn)) {
+    header('Location: listaProdutos.php?Alteracao realizada com sucesso');
   }
   else{
     echo '
@@ -184,7 +179,7 @@ if(isset($_POST['salvar'])){
       <input type="text" autocomplete="off" name="nomeProduto" value="<?php if(isset($_GET['idProduto'])){echo $nomeProduto;} ?>">
 
       <p class="tituloCampo">Categoria</p>
-      <select class="" name="categoria">
+      <select class="" name="idCategoria">
         <option value="">Selecione uma opção</option>
         <?php
           $query = "SELECT * FROM categorias WHERE removido != 'sim' ORDER BY nomeCategoria";
@@ -199,6 +194,12 @@ if(isset($_POST['salvar'])){
         ?>
       </select>
 
+      <p class="tituloCampo">Código do Produto</p>
+      <input type="number" autocomplete="off" name="codigoProduto" value="<?php if(isset($_GET['idProduto'])){echo $codigoProduto;} ?>">
+
+      <p class="tituloCampo">Imagem</p>
+      <input type="file" name="fotoProduto" value="">
+
       <p class="tituloCampo">Marca do Produto</p>
       <input type="text" autocomplete="off" name="marcaProduto" value="<?php if(isset($_GET['idProduto'])){echo $marcaProduto;} ?>">
 
@@ -208,29 +209,20 @@ if(isset($_POST['salvar'])){
       <p class="tituloCampo">Preço em Promoção</p>
       <input type="number" autocomplete="off" step=",01" name="precoPromocao" value="<?php if(isset($_GET['idProduto'])){echo $precoPromocao;} ?>">
 
-      <p class="tituloCampo">Cor</p>
-      <input type="text" autocomplete="off" placeholder="ex.: preto como detalhes brancos" name="cor" value="<?php if(isset($_GET['idProduto'])){echo $cor;} ?>">
+      <p class="tituloCampo">Peso do Produto</p>
+      <input type="number" autocomplete="off" step=",01" name="pesoProduto" value="<?php if(isset($_GET['idProduto'])){echo $pesoProduto;} ?>">
 
-      <p class="tituloCampo">Tamanho</p>
-      <input type="text" autocomplete="off" placeholder="ex.: P, M, G. (se for calçado colocar o nº)" name="tamanho" value="<?php if(isset($_GET['idProduto'])){echo $tamanho;} ?>">
+      <p class="tituloCampo">Localização do Produto</p>
+      <input type="text" autocomplete="off" placeholder="ex.: Corredor 6, na área de limpeza." name="localizacaoProduto" value="<?php if(isset($_GET['idProduto'])){echo $localizacaoProduto;} ?>">
 
-      <p class="tituloCampo">Estado</p>
-      <input type="text" autocomplete="off" placeholder="ex.: usado, novo, etc." name="estado" value="<?php if(isset($_GET['idProduto'])){echo $tamanho;} ?>">
+      <p class="tituloCampo">Avaliação sobre o Produto</p>
+      <input type="text" autocomplete="off" step=",01" name="avaliacaoProduto" value="<?php if(isset($_GET['idProduto'])){echo $avaliacaoProduto;} ?>">
 
-      <p class="tituloCampo">Descrição</p>
-      <textarea name="descricao" class="tinymce" rows="15" cols="80"><?php if(isset($_GET['idProduto'])){echo $descricao;} ?></textarea>
+      <p class="tituloCampo">Descrição do Produto</p>
+      <textarea name="descricaoProduto" class="tinymce" rows="15" cols="80"><?php if(isset($_GET['idProduto'])){echo $descricaoProduto;} ?></textarea>
 
-      <p class="tituloCampo">Status</p>
-      <select class="" name="status">
-        <option value="">Selecione uma opção</option>
-        <option value="postado" <?php if(isset($status) AND $status == 'postado'){echo 'selected';} ?>>Postado</option>
-        <option value="pendente" <?php if(isset($status) AND $status == 'pendente'){echo 'selected';} ?>>Pendente</option>
-        <option value="reservado" <?php if(isset($status) AND $status == 'reservado'){echo 'selected';} ?>>Reservado</option>
-        <option value="vendido" <?php if(isset($status) AND $status == 'vendido'){echo 'selected';} ?>>Vendido</option>
-      </select>
-
-      <p class="tituloCampo">Imagem</p>
-      <input type="file" name="imagem" value="">
+      <p class="tituloCampo">Destaque</p>
+      <input type="text" autocomplete="off" step=",01" name="destaqueProduto" value="<?php if(isset($_GET['idProduto'])){echo $destaqueProduto;} ?>">
 
       <br><br><br><br>
       <input type="submit" class="btnSalvar" name="<?php if(isset($_GET['idProduto'])){echo 'salvar';}else{echo 'cadastrar';} ?>" value="Salvar">
