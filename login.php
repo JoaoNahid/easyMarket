c0226d941798e24ea15547f5fb0918d2e7f1ae5e
Gilmorius <joaoeduardo.008@gmail.com>
2021-08-11 16:44:41 -0300

    início Login cliente

--- login.php
+++ login.php
@@ -3,36 +3,99 @@ include('includes/header.php');
 include('conexao.php');
 include('sist/includes/mostrarErros.php');
 
+// INÍCIO CADASTRO
 if (isset($_POST['cadastrarCliente'])) {
   $nomeCliente = htmlspecialchars($_POST['nomeCliente'], ENT_QUOTES, 'utf-8');
   $emailCliente = htmlspecialchars($_POST['emailCliente'], ENT_QUOTES, 'utf-8');
   $senhaCliente = htmlspecialchars($_POST['senhaCliente'], ENT_QUOTES, 'utf-8');
-  $telefoneCliente = htmlspecialchars($_POST['telefoneCliente'], ENT_QUOTES, 'utf-8');
   $confirmarSenha = htmlspecialchars($_POST['confirmarSenha'], ENT_QUOTES, 'utf-8');
+  $telefoneCliente = htmlspecialchars($_POST['telefoneCliente'], ENT_QUOTES, 'utf-8');
 
-  if($senhaCliente === $confirmarSenha){
-    $senhaCliente = md5($senhaCliente);
-    echo $query = "INSERT INTO clientes  (nomeCliente, emailCliente, senhaCliente, telefoneCliente) VALUES ('$nomeCliente','$emailCliente', '$senhaCliente', '$telefoneCliente', ' ')";
-    $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
-    if (mysqli_affected_rows($conn)) {
-      //header('Location: index.php?Cadastro realizado com sucesso');
+  if ($nomeCliente != '' AND $emailCliente != '' AND $senhaCliente != '' AND $telefoneCliente != '' AND $confirmarSenha != '') {
+
+    $query = "SELECT emailCliente FROM usuarios WHERE emailCliente='$emailCliente'";
+    $result = mysqli_query($conn, $query);
+    if(mysqli_num_rows($result) > 0){
+      if($senhaCliente === $confirmarSenha){
+        $senhaCliente = md5($senhaCliente);
+        echo $query = "INSERT INTO clientes  (nomeCliente, emailCliente, senhaCliente, telefoneCliente, removido) VALUES ('$nomeCliente','$emailCliente', '$senhaCliente', '$telefoneCliente', ' ')";
+        $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+        if (mysqli_affected_rows($conn)) {
+          header('Location: index.php?Cadastro realizado com sucesso');
+        }
+        else{
+          echo '
+            <script>
+              window.alert("Erro ao cadastrar, tente novamente!")
+            </script>
+          ';
+        }
+      }
+      else {
+        echo '
+          <script>
+            window.alert("As senhas são diferentes.")
+          </script>
+        ';
+        header('Location: login.php');
+      }
     }
     else{
       echo '
         <script>
-          window.alert("Erro ao cadastrar, tente novamente!")
+          window.alert("E-mail já cadastrado.")
+          window.location.href="login.php";
         </script>
       ';
     }
   }
-  else {
+  else{
     echo '
       <script>
-        window.alert("As senhas não conferem.")
+        window.alert("Preencha todos os campos.")
       </script>
     ';
   }
 }
+// FIM CADASTRO
+
+// INÍCIO LOGIN
+
+if(isset($_POST['efetuarLogin'])){
+  session_start();
+  $login = htmlspecialchars($_POST['login'], ENT_QUOTES, 'utf-8');
+  $senhaLogin = htmlspecialchars($_POST['senhaLogin'], ENT_QUOTES, 'utf-8');
+
+  $query = "SELECT nomeCliente, emailCliente FROM clientes WHERE emailCliente='$login' OR nomeCliente='$login'";
+  $result = mysqli_query($conn, $query);
+  if (mysqli_num_rows($result) > 0) {
+    $query = "SELECT * FROM clientes WHERE senhaCliente='$senhaLogin'";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) > 0){
+      while($row = mysqli_fetch_assoc($result)){
+        $_SESSION['cliente'] = $row['nomeCliente'];
+        $_SESSION['idCliente'] = $row['idCliente'];
+      }
+      $_SESSION['logado'] = true;
+    }
+    else{
+      echo '
+        <script>
+          window.alert("Email e senha incorretos.")
+        </script>
+      ';
+    }
+  }
+  else{
+    echo '
+      <script>
+        window.alert("Email não encontrado.")
+      </script>
+    ';
+  }
+}
+
+// FIM LOGIN
 ?>
     <div class="yellow_bg">
       <div class="container">
@@ -62,7 +125,7 @@ if (isset($_POST['cadastrarCliente'])) {
                           </div>
 
                           <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                            <input class="form-control" placeholder="Senha" type="password" name="senha">
+                            <input class="form-control" placeholder="Senha" type="password" name="senhaLogin">
                           </div>
 
                           <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
@@ -82,23 +145,23 @@ if (isset($_POST['cadastrarCliente'])) {
                     <div class="row">
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <input class="form-control" placeholder="Nome" type="text" name="nomeCliente">
+                        <input class="form-control" required placeholder="Nome" type="text" name="nomeCliente">
                       </div>
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <input class="form-control" placeholder="Email" type="text" name="emailCliente">
+                        <input class="form-control" required placeholder="Email" type="text" name="emailCliente">
                       </div>
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <input class="form-control" placeholder="Teleone" type="text" name="telefoneCliente">
+                        <input class="form-control" required placeholder="Teleone" type="text" name="telefoneCliente">
                       </div>
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <input class="form-control" placeholder="Senha" type="password" name="senhaCliente">
+                        <input class="form-control" required placeholder="Senha" type="password" name="senhaCliente">
                       </div>
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <input class="form-control" placeholder="Confirmar senha" type="password" name="confirmarSenha">
+                        <input class="form-control" required placeholder="Confirmar senha" type="password" name="confirmarSenha">
                       </div>
 
                       <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
