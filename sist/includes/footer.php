20f0bb055f097e3e17579db3ca1daa15ebc215b2
Gilmorius <joaoeduardo.008@gmail.com>
2021-08-23 14:28:25 -0300

    erro abrir config em sistema consertado 23/08

--- sist/includes/footer.php
+++ sist/includes/footer.php
@@ -2,5 +2,12 @@
 
 <script src="js/manterMenuAberto.js"></script>
 <script src="https://igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js"></script>
+<script>
+  function abrirMenuConfig(){
+    var element = document.getElementById('menuConfig');
+    element.classList.toggle('clickOnConfig');
+  }
+
+</script>
 
 </html>
